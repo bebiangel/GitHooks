d40f96fe9328a9724e73131c7657c97d6cd5f3a2
jeus.han <jeus.han@goodoc.co.kr>
2019-10-04 15:22:07 +0900

    edit shell

--- scripts/post_merge.sh
+++ scripts/post_merge.sh
@@ -10,7 +10,6 @@ echo "1 : " $(git rev-parse MERGE_HEAD)
 echo "2 : " $(git for-each-ref | grep ^$(git rev-parse MERGE_HEAD))
 #echo "2 : "$(expr $(git for-each-ref | grep ^$(git rev-parse MERGE_HEAD)) | grep -E -o $VERSION_REGEX)
 #TARGET_BRANCH_VERSION=$(expr $BRANCH_NAME | grep -E -o $VERSION_REGEX)
-#echo $TARGET_BRANCH_VERSION
 
 #
 if [ $BRANCH_NAME != "master" ]; then
