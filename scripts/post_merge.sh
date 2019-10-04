0d2fff9e366fc62756caea0e223cef948c030e8c
jeus.han <jeus.han@goodoc.co.kr>
2019-10-04 15:12:11 +0900

    edit

--- scripts/post_merge.sh
+++ scripts/post_merge.sh
@@ -5,14 +5,12 @@ BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 VERSION_REGEX="[0-9]+(\.[0-9]+)+"
 
 echo 'CURRENT BRANCH_NAME: ' "${BRANCH_NAME}"
-echo "GIT_COMMIT_TO_MASTER: " "${GIT_COMMIT_TO_MASTER}"
-
 
 echo "1 : " $(git rev-parse MERGE_HEAD)
-echo "2 : "$(git for-each-ref | grep ^$(git rev-parse MERGE_HEAD))
-echo "2 : "$(expr $(git for-each-ref | grep ^$(git rev-parse MERGE_HEAD)) | grep -E -o $VERSION_REGEX)
-TARGET_BRANCH_VERSION=$(expr $BRANCH_NAME | grep -E -o $VERSION_REGEX)
-echo $TARGET_BRANCH_VERSION
+echo "2 : " $(git for-each-ref | grep ^$(git rev-parse MERGE_HEAD))
+#echo "2 : "$(expr $(git for-each-ref | grep ^$(git rev-parse MERGE_HEAD)) | grep -E -o $VERSION_REGEX)
+#TARGET_BRANCH_VERSION=$(expr $BRANCH_NAME | grep -E -o $VERSION_REGEX)
+#echo $TARGET_BRANCH_VERSION
 
 #
 if [ $BRANCH_NAME != "master" ]; then
