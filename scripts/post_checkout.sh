93ec53804890569f2e7015b7bd04dadd49712e92
jaewoo <rossoneri1012@gmail.com>
2019-10-09 14:32:27 +0900

    ≈

--- scripts/post_checkout.sh
+++ scripts/post_checkout.sh
@@ -3,29 +3,31 @@
 VERSION_REGEX="[0-9]+(\.[0-9]+)+"
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 BRANCH_VERSION=$(expr $BRANCH_NAME | grep -E -o $VERSION_REGEX)
+COMMIT_MESSAGE='feat:package.json 버전 업데이트'
 
-if [ $BRANCH_NAME == 'master' ]; then
-    echo 'master 브랜치입니다.'
-    exit;
+if [ "$BRANCH_NAME" == 'master' ]; then
+  echo 'master 브랜치입니다.'
+  exit
 fi
 
-if [ "x"${BRANCH_VERSION} == "x" ]; then
+if [ "x""$BRANCH_VERSION" == "x" ]; then
   echo '브랜치 버전이 존재하지 않습니다.'
-  exit;
+  exit
 fi
 
 PACKAGE_VERSION=$(npm run version --silent)
 
-if [ x$BRANCH_VERSION == x ]; then
+if [ x"$BRANCH_VERSION" == x ]; then
   echo '브랜치 버전이 맞지 않습니다.'
   exit
 fi
 if [[ $PACKAGE_VERSION != $BRANCH_VERSION ]]; then
   echo '패키지 버전을 업데이트 합니다.'
-  npm --no-git-tag-version version ${BRANCH_VERSION}
+  npm --no-git-tag-version version "${BRANCH_VERSION}"
+  git add .
+  git commit -a "$COMMIT_MESSAGE"
 else
   {
     echo '패키지 버전과 브랜치 버전이 같습니다.'
   }
 fi
-
